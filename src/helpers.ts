dc5068f5f3321c156a8de61e2cb4345fd676fa71
ssallam <travis@travis-ci.org>
2021-01-18 22:12:29 +0100

    fix ocean address

--- src/helpers.ts
+++ src/helpers.ts
@@ -35,7 +35,7 @@ export const ENABLE_DEBUG = true
 const network = dataSource.network()
 
 export const OCEAN: string =
-  network === 'mainnet'
+  network == 'mainnet'
     ? '0x967da4048cd07ab37855c090aaf366e4ce1b9f48'
     : '0x8967BCF84170c91B0d24D4302C2376283b0B3a07'
 
