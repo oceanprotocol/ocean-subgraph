f1feb3c2163bbc317829016e537bb2f59fc06d3e
alexcos20 <alex.coseru@gmail.com>
2021-04-10 16:26:04 -0700

    fix non ocean pools

--- src/mappings/metadata.ts
+++ src/mappings/metadata.ts
@@ -6,33 +6,34 @@ import { Datatoken, MetadataUpdate } from '../@types/schema'
 export function handleMetadataEvent(
   event: ethereum.Event,
   dtAddress: string,
-  updatedBy: string
+  updatedBy: string,
+  created: boolean
 ): void {
   const datatoken = Datatoken.load(dtAddress)
-
-  const tx = event.transaction.hash
-  const id = tx.toHexString().concat('-').concat(dtAddress)
-  const metadataUpdate = new MetadataUpdate(id)
-  metadataUpdate.tx = tx
-  metadataUpdate.block = event.block.number.toI32()
-  metadataUpdate.timestamp = event.block.timestamp.toI32()
-  metadataUpdate.datatokenAddress = dtAddress
-  metadataUpdate.userAddress = updatedBy
-  metadataUpdate.datatokenId = dtAddress
-
-  metadataUpdate.save()
-
-  datatoken.metadataUpdateCount = datatoken.metadataUpdateCount.plus(
-    BigInt.fromI32(1)
-  )
-  datatoken.save()
+  if(datatoken){
+    const tx = event.transaction.hash
+    const id = tx.toHexString().concat('-').concat(dtAddress)
+    const metadataUpdate = new MetadataUpdate(id)
+    metadataUpdate.tx = tx
+    metadataUpdate.block = event.block.number.toI32()
+    metadataUpdate.timestamp = event.block.timestamp.toI32()
+    metadataUpdate.datatokenAddress = dtAddress
+    metadataUpdate.userAddress = updatedBy
+    metadataUpdate.datatokenId = dtAddress
+    metadataUpdate.save()
+    datatoken.metadataUpdateCount = datatoken.metadataUpdateCount.plus(
+      BigInt.fromI32(1)
+    )
+    datatoken.save()
+  }
 }
 
 export function handleMetadataUpdated(event: MetadataUpdated): void {
   handleMetadataEvent(
     event,
     event.params.dataToken.toHexString(),
-    event.params.updatedBy.toHexString()
+    event.params.updatedBy.toHexString(),
+    false
   )
 }
 
@@ -40,6 +41,7 @@ export function handleMetadataCreated(event: MetadataCreated): void {
   handleMetadataEvent(
     event,
     event.params.dataToken.toHexString(),
-    event.params.createdBy.toHexString()
+    event.params.createdBy.toHexString(),
+    true
   )
 }
