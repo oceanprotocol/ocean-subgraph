db6c30fee566e388e637310e572572f2beb8c4e8
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-05-06 12:30:57 +0300

    creating dispenser mapping

---
+++ src/mappings/dispenser.ts
@@ -0,0 +1,8 @@
+import { TokensDispensed } from '../@types/templates/Dispenser/Dispenser'
+import { Datatoken } from '../@types/schema'
+
+export function handleTokensDispensed(event: TokensDispensed): void {
+  const tokenId = event.address.toHex()
+  const dispensedToken = Datatoken.load(tokenId)
+  dispensedToken.save()
+}
