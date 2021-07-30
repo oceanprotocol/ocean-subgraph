2c652afdd16229d34ebf2d5a0b6e9deaad9cee46
mihaisc <mihai@oceanprotocol.com>
2021-07-30 13:43:30 +0300

    test

--- scripts/waitforcontracts.sh
+++ scripts/waitforcontracts.sh
@@ -1,8 +1,8 @@
-while [ ! -f "${HOME}/.ocean/ocean-contracts/artifacts/ready" ]; do
+while [ ! -f "$HOME/.ocean/ocean-contracts/artifacts/ready" ]; do
   sleep 2
 done
 
 cat "barge/start_ocean.log"
-ls -lh "${HOME}/.ocean/ocean-contracts/"
-ls -lh "${HOME}/.ocean/ocean-contracts/artifacts/"
-cat "${HOME}/.ocean/ocean-contracts/artifacts/address.json"
+ls -lh "$HOME/.ocean/ocean-contracts/"
+ls -lh "$HOME/.ocean/ocean-contracts/artifacts/"
+cat "$HOME/.ocean/ocean-contracts/artifacts/address.json"
