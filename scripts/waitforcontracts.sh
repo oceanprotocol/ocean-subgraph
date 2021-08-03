8a7426110fc0d70a18b71127ab24b67120153a62
mihaisc <mihai@oceanprotocol.com>
2021-08-03 09:57:25 +0300

    remove unused script

--- scripts/waitforcontracts.sh
+++
@@ -1,8 +0,0 @@
-while [ ! -f "$HOME/.ocean/ocean-contracts/artifacts/ready" ]; do
-  sleep 2
-done
-
-cat "barge/start_ocean.log"
-ls -lh "$HOME/.ocean/ocean-contracts/"
-ls -lh "$HOME/.ocean/ocean-contracts/artifacts/"
-cat "$HOME/.ocean/ocean-contracts/artifacts/address.json"
