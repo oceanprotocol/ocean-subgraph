d5036fcb1129502df17bbd0dd14dbb2d40b19133
mihaisc <mihai@oceanprotocol.com>
2022-02-15 16:27:38 +0200

    liquidty and swap

--- src/mappings/utils/globalUtils.ts
+++ src/mappings/utils/globalUtils.ts
@@ -1,5 +1,10 @@
 import { BigDecimal } from '@graphprotocol/graph-ts'
-import { GlobalStatistic } from '../../@types/schema'
+import {
+  GlobalStatistic,
+  GlobalTotalFixedSwapPair,
+  GlobalTotalLiquidityPair,
+  GlobalTotalPoolSwapPair
+} from '../../@types/schema'
 
 const GLOBAL_ID = '1'
 
@@ -47,8 +52,50 @@ export function addPool(): void {
   globalStats.save()
 }
 
-export function addSwap(tokenAddress: string, value: BigDecimal): void {
-  const globalStats = getGlobalStats()
+export function addPoolSwap(tokenAddress: string, value: BigDecimal): void {
+  let poolSwapPair = GlobalTotalPoolSwapPair.load(tokenAddress)
+  if (!poolSwapPair) {
+    poolSwapPair = new GlobalTotalPoolSwapPair(tokenAddress)
+    poolSwapPair.globalStatistic = GLOBAL_ID
+    poolSwapPair.token = tokenAddress
+  }
+  poolSwapPair.value = poolSwapPair.value.plus(value)
 
-  globalStats.save()
+  poolSwapPair.save()
+}
+
+export function addFixedSwap(tokenAddress: string, value: BigDecimal): void {
+  let fixedSwapPair = GlobalTotalFixedSwapPair.load(tokenAddress)
+  if (!fixedSwapPair) {
+    fixedSwapPair = new GlobalTotalFixedSwapPair(tokenAddress)
+    fixedSwapPair.globalStatistic = GLOBAL_ID
+    fixedSwapPair.token = tokenAddress
+  }
+  fixedSwapPair.value = fixedSwapPair.value.plus(value)
+
+  fixedSwapPair.save()
+}
+
+export function addLiquidity(tokenAddress: string, value: BigDecimal): void {
+  let liquidityPair = GlobalTotalLiquidityPair.load(tokenAddress)
+  if (!liquidityPair) {
+    liquidityPair = new GlobalTotalLiquidityPair(tokenAddress)
+    liquidityPair.globalStatistic = GLOBAL_ID
+    liquidityPair.token = tokenAddress
+  }
+  liquidityPair.value = liquidityPair.value.plus(value)
+
+  liquidityPair.save()
+}
+
+export function removeLiquidity(tokenAddress: string, value: BigDecimal): void {
+  let liquidityPair = GlobalTotalLiquidityPair.load(tokenAddress)
+  if (!liquidityPair) {
+    liquidityPair = new GlobalTotalLiquidityPair(tokenAddress)
+    liquidityPair.globalStatistic = GLOBAL_ID
+    liquidityPair.token = tokenAddress
+  }
+  liquidityPair.value = liquidityPair.value.minus(value)
+
+  liquidityPair.save()
 }
