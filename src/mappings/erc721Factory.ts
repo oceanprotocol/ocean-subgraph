aa9cd1471406373b1d48762907f8fbe5a50bbe7c
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-08-02 10:12:23 +0300

    Updating templateId

--- src/mappings/erc721Factory.ts
+++ src/mappings/erc721Factory.ts
@@ -1,11 +1,15 @@
-import { NFTCreated, TokenCreated } from '../@types/ERC721Factory/ERC721Factory'
+import {
+  NFTCreated,
+  TokenCreated,
+  ERC721Factory
+} from '../@types/ERC721Factory/ERC721Factory'
 import { decimal } from './utils/constants'
 import { weiToDecimal } from './utils/generic'
 
 import { getUser } from './utils/userUtils'
 import { getToken, getNftToken } from './utils/tokenUtils'
 import { addDatatoken, getTemplates } from './utils/globalUtils'
-import { log } from '@graphprotocol/graph-ts'
+import { log, BigInt } from '@graphprotocol/graph-ts'
 
 export function handleNftCreated(event: NFTCreated): void {
   // const nft = new Nft(event.params.newTokenAddress.toHexString())
@@ -43,24 +47,39 @@ export function handleNewToken(event: TokenCreated): void {
   token.decimals = 18
   token.supply = decimal.ZERO
   token.cap = weiToDecimal(event.params.cap.toBigDecimal(), 18)
-  const templateAddress = event.params.templateAddress
-  let existingContracts: string[]
-  const templates = getTemplates()
-  if (!templates.fixedRateTemplates) existingContracts = []
-  else existingContracts = templates.ssTemplates as string[]
-
+  const eventTemplateAddress = event.params.templateAddress
+    .toHexString()
+    .toLowerCase()
+  const contract = ERC721Factory.bind(event.address)
+  const templateCount = contract.try_getCurrentTemplateCount()
+  if (templateCount.reverted) return
+  const templateCountNum = templateCount.value.toI32()
   log.info(
-    '\n\n\n**********\n\n\n***********\n\n\nAddress is template:\n{}\n\n\n************\n\n\n*********\n\n\n',
-    [existingContracts[0]]
+    '\n\n\n**********\n\n\n***********\n\n\ntemplateCountNum: \n[0]:\n{}\n\n\n************\n\n\n*********\n\n\n',
+    [templateCountNum.toString()]
   )
-  log.info(
-    '\n\n\n**********\n\n\n***********\n\n\nAddress is template:\n{}\n\n\n************\n\n\n*********\n\n\n',
-    [existingContracts[1]]
-  )
-  log.info(
-    '\n\n\n**********\n\n\n***********\n\n\nChecking if templates work! \n[0]:\n{}\n\n\n************\n\n\n*********\n\n\n',
-    [templateAddress.toHexString()]
-  )
+  for (let i = 0; i < templateCountNum; i++) {
+    const template = contract.try_getTokenTemplate(BigInt.fromI32(1 + i))
+    if (template.reverted) return
+    const templateAddress = template.value.templateAddress
+      .toHexString()
+      .toLowerCase()
+    log.info(
+      '\n\n\n**********\n\n\n***********\n\n\nChecking if templates work! \n[1]:\n{}\n\n\n************\n\n\n*********\n\n\n',
+      [templateAddress]
+    )
+    log.info(
+      '\n\n\n**********\n\n\n***********\n\n\nCompare with: \n[2]:\n{}\n\n\n************\n\n\n*********\n\n\n',
+      [eventTemplateAddress]
+    )
+    if (templateAddress == eventTemplateAddress) {
+      log.info(
+        '\n\n\n**********\n\n\n***********\n\n\nMATCH - templates work! \n[0]:\n{}\n\n\n************\n\n\n*********\n\n\n',
+        [i.toString()]
+      )
+      token.templateId = 1 + i
+    }
+  }
 
   token.save()
   addDatatoken()
