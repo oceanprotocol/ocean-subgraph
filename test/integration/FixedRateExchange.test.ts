5e60df5a64b9e216e79e96a7c1cb5c6b0fa822c1
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-08-02 18:40:37 +0300

    Splitting up tests

--- test/integration/FixedRateExchange.test.ts
+++ test/integration/FixedRateExchange.test.ts
@@ -1,17 +1,13 @@
 import {
   Erc20CreateParams,
-  ProviderInstance,
-  Nft,
   NftFactory,
   NftCreateData,
-  getHash,
   sleep,
   FreCreationParams,
   ZERO_ADDRESS
 } from '@oceanprotocol/lib'
 import { assert } from 'chai'
 import Web3 from 'web3'
-import { SHA256 } from 'crypto-js'
 import { homedir } from 'os'
 import fs from 'fs'
 import { fetch } from 'cross-fetch'
@@ -26,54 +22,15 @@ const data = JSON.parse(
 )
 
 const addresses = data.development
-// const aquarius = new Aquarius('http://127.0.0.1:5000')
 const web3 = new Web3('http://127.0.0.1:8545')
 
-const providerUrl = 'http://172.15.0.4:8030'
 const subgraphUrl =
   'http://127.0.0.1:9000/subgraphs/name/oceanprotocol/ocean-subgraph'
-const assetUrl = [
-  {
-    type: 'url',
-    url: 'https://raw.githubusercontent.com/oceanprotocol/testdatasets/main/shs_dataset_test.txt',
-    method: 'GET'
-  }
-]
-const ddo = {
-  '@context': ['https://w3id.org/did/v1'],
-  id: 'did:op:efba17455c127a885ec7830d687a8f6e64f5ba559f8506f8723c1f10f05c049c',
-  version: '4.0.0',
-  chainId: 4,
-  nftAddress: '0x0',
-  metadata: {
-    created: '2021-12-20T14:35:20Z',
-    updated: '2021-12-20T14:35:20Z',
-    type: 'dataset',
-    name: 'dfgdfgdg',
-    description: 'd dfgd fgd dfg dfgdfgd dfgdf',
-    tags: [''],
-    author: 'dd',
-    license: 'https://market.oceanprotocol.com/terms',
-    additionalInformation: {
-      termsAndConditions: true
-    }
-  },
-  services: [
-    {
-      id: 'notAnId',
-      type: 'access',
-      files: '',
-      datatokenAddress: '0xa15024b732A8f2146423D14209eFd074e61964F3',
-      serviceEndpoint: 'https://providerv4.rinkeby.oceanprotocol.com',
-      timeout: 0
-    }
-  ]
-}
 
 describe('Fixed Rate Exchange tests', async () => {
-  const nftName = 'testNFT'
-  const nftSymbol = 'TST'
-  const tokenURI = 'https://oceanprotocol.com/nft/'
+  const nftName = 'test-Fixed-Price-NFT'
+  const nftSymbol = 'TST-FIXED'
+  const tokenURI = 'https://oceanprotocol.com/nft/fixed'
   const cap = '10000'
   const feeAmount = '0.2'
   const price = '123'
@@ -83,7 +40,7 @@ describe('Fixed Rate Exchange tests', async () => {
   let baseTokenAddress: string
   let marketPlaceFeeAddress: string
   let fixedRateId: string
-  let nft: Nft
+  let dt
   let Factory: NftFactory
   let factoryAddress: string
   let accounts: string[]
@@ -92,9 +49,11 @@ describe('Fixed Rate Exchange tests', async () => {
   let nftAddress: string
   let time: number
   let blockNumber: number
+  let exchangeContract: string
+  let exchangeId: string
+  let transactionHash: string
 
   before(async () => {
-    nft = new Nft(web3)
     factoryAddress = addresses.ERC721Factory.toLowerCase()
     fixedRateAddress = addresses.FixedPrice.toLowerCase()
     baseTokenAddress = addresses.MockDAI.toLowerCase()
@@ -104,7 +63,7 @@ describe('Fixed Rate Exchange tests', async () => {
     marketPlaceFeeAddress = accounts[1].toLowerCase()
   })
 
-  it('Deploying a Fixed Rate Exchange with DAI (18 Decimals)', async () => {
+  it('Deploying a Fixed Rate Exchange & Test NFT Fields', async () => {
     const date = new Date()
     time = Math.floor(date.getTime() / 1000)
     blockNumber = await web3.eth.getBlockNumber()
@@ -145,12 +104,13 @@ describe('Fixed Rate Exchange tests', async () => {
       erc20Params,
       fixedRateParams
     )
+    transactionHash = result.transactionHash.toLowerCase()
     erc721Address = result.events.NFTCreated.returnValues[0]
     datatokenAddress = result.events.TokenCreated.returnValues[0].toLowerCase()
 
-    const exchangeContract =
+    exchangeContract =
       result.events.NewFixedRate.returnValues.exchangeContract.toLowerCase()
-    const exchangeId =
+    exchangeId =
       result.events.NewFixedRate.returnValues.exchangeId.toLowerCase()
 
     fixedRateId = `${exchangeContract}-${exchangeId}`
@@ -219,7 +179,8 @@ describe('Fixed Rate Exchange tests', async () => {
     assert(nft.block >= blockNumber, 'incorrect value for: block')
     assert(nft.block < blockNumber + 50, 'incorrect value for: block')
     assert(nft.orderCount === '0', 'incorrect value for: orderCount')
-
+  })
+  it('Test DT Fields after deploying Fixed rate exchange', async () => {
     // Check Datatoken Values
 
     const dtQuery = {
@@ -292,13 +253,9 @@ describe('Fixed Rate Exchange tests', async () => {
       body: JSON.stringify(dtQuery)
     })
     await sleep(2000)
-    const dt = (await dtResponse.json()).data.token
-    const fixed = dt.fixedRateExchanges[0]
+    dt = (await dtResponse.json()).data.token
 
     const dtTx: TransactionReceipt = await web3.eth.getTransactionReceipt(dt.tx)
-    const fixedTx: TransactionReceipt = await web3.eth.getTransactionReceipt(
-      fixed.tx
-    )
 
     assert(dt.id === datatokenAddress, 'incorrect value for: id')
     assert(dt.symbol, 'incorrect value for: symbol')
@@ -345,7 +302,12 @@ describe('Fixed Rate Exchange tests', async () => {
       'incorrect value for: lastPriceToken'
     )
     assert(dt.lastPriceValue === '0', 'incorrect value for: lastPriceValue')
-
+  })
+  it('Deploying a Fixed Rate Exchange & Test fixedRateExchanges Fields', async () => {
+    const fixed = dt.fixedRateExchanges[0]
+    const fixedTx: TransactionReceipt = await web3.eth.getTransactionReceipt(
+      fixed.tx
+    )
     // Test Fixed Rate Exchange Values
     assert(fixed.id === fixedRateId, 'incorrect value for: id')
     assert(fixed.contract === exchangeContract, 'incorrect value for: contract')
@@ -385,7 +347,7 @@ describe('Fixed Rate Exchange tests', async () => {
     assert(fixed.withMint === null, 'incorrect value for: withMint')
     assert(fixed.isMinter === null, 'incorrect value for: isMinter')
     assert(
-      fixed.updates[0].id === `${result.transactionHash}-${fixedRateId}`,
+      fixed.updates[0].id === `${transactionHash}-${fixedRateId}`,
       'incorrect value for: updates.id'
     )
     assert(fixed.swaps, 'incorrect value for: swaps')
@@ -397,7 +359,7 @@ describe('Fixed Rate Exchange tests', async () => {
       fixed.createdTimestamp < time + 5,
       'incorrect value for: createdTimestamp'
     )
-    assert(fixed.tx === result.transactionHash, 'incorrect value for: tx')
+    assert(fixed.tx === transactionHash, 'incorrect value for: tx')
     assert(fixed.block >= blockNumber, 'incorrect value for: block')
     assert(fixed.block < blockNumber + 50, 'incorrect value for: block')
     assert(
@@ -412,105 +374,10 @@ describe('Fixed Rate Exchange tests', async () => {
     assert(fixedTx.to === factoryAddress, 'incorrect value for: tx')
   })
 
-  it('Update metadata', async () => {
-    // create the files encrypted string
-    let providerResponse = await ProviderInstance.encrypt(assetUrl, providerUrl)
-    ddo.services[0].files = await providerResponse
-    ddo.services[0].datatokenAddress = datatokenAddress
-    // update ddo and set the right did
-    ddo.nftAddress = erc721Address
-    const chain = await web3.eth.getChainId()
-    ddo.id =
-      'did:op:' +
-      SHA256(web3.utils.toChecksumAddress(erc721Address) + chain.toString(10))
-
-    providerResponse = await ProviderInstance.encrypt(ddo, providerUrl)
-    const encryptedResponse = await providerResponse
-    const metadataHash = getHash(JSON.stringify(ddo))
-    await nft.setMetadata(
-      erc721Address,
-      publisher,
-      0,
-      providerUrl,
-      '',
-      '0x2',
-      encryptedResponse,
-      '0x' + metadataHash
-    )
-
-    // graph tests here
-    await sleep(2000)
-    const query = {
-      query: `query {
-              nft(id:"${nftAddress}"){    
-                id,
-                symbol,
-                name,
-                tokenUri,
-                owner,
-                creator,
-                address,
-                providerUrl,
-                assetState,
-                managerRole,
-                erc20DeployerRole,
-                storeUpdateRole,
-                metadataRole,
-                template,
-                transferable,
-                createdTimestamp,
-                tx,
-                block,
-                orderCount}}`
-    }
-    const response = await fetch(subgraphUrl, {
-      method: 'POST',
-      body: JSON.stringify(query)
-    })
-    const updatedNft = (await response.json()).data.nft
-    const tx: TransactionReceipt = await web3.eth.getTransactionReceipt(
-      updatedNft.tx
-    )
-    assert(updatedNft.id === nftAddress, 'incorrect value for: id')
-    assert(updatedNft.symbol === nftSymbol, 'incorrect value for: symbol')
-    assert(updatedNft.name === nftName, 'incorrect value for: name')
-    assert(updatedNft.tokenUri === tokenURI, 'incorrect value for: tokenUri')
-    assert(updatedNft.owner === publisher, 'incorrect value for: owner')
-    assert(updatedNft.creator === publisher, 'incorrect value for: creator')
-    assert(
-      updatedNft.managerRole[0] === publisher,
-      'incorrect value for: managerRole'
-    )
-    assert(
-      updatedNft.erc20DeployerRole[0] === factoryAddress,
-      'incorrect value for: erc20DeployerRole'
-    )
-    assert(
-      updatedNft.storeUpdateRole === null,
-      'incorrect value for: storeUpdateRole'
-    )
-    assert(
-      updatedNft.metadataRole === null,
-      'incorrect value for: metadataRole'
-    )
-    assert(updatedNft.template === '', 'incorrect value for: template')
-    assert(
-      updatedNft.transferable === true,
-      'incorrect value for: transferable'
-    )
-    assert(
-      updatedNft.createdTimestamp >= time,
-      'incorrect value for: createdTimestamp'
-    )
-    assert(
-      updatedNft.createdTimestamp < time + 5,
-      'incorrect value for: createdTimestamp'
-    )
-    assert(tx.from === publisher, 'incorrect value for: tx')
-    assert(tx.to === factoryAddress, 'incorrect value for: tx')
-    assert(tx.blockNumber >= blockNumber, 'incorrect value for: tx')
-    assert(updatedNft.block >= blockNumber, 'incorrect value for: block')
-    assert(updatedNft.block < blockNumber + 50, 'incorrect value for: block')
-    assert(updatedNft.orderCount === '0', 'incorrect value for: orderCount')
-  })
+  // it('Update Fixed Rate Price', async () => {
+  //   await fixedRate.setRate(exchangeOwner, exchangeId, '2')
+  //   expect(await fixedRate.getRate(exchangeId)).to.equal('2')
+  //   await fixedRate.setRate(exchangeOwner, exchangeId, '1')
+  //   expect(await fixedRate.getRate(exchangeId)).to.equal('1')
+  // })
 })
