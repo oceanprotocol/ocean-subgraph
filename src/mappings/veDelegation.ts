6078c56001b042ac9513737c2a10e4e9710c0120
trizin <25263018+trizin@users.noreply.github.com>
2022-08-09 14:49:26 +0000

    Add `handleDelegation`

--- src/mappings/veDelegation.ts
+++ src/mappings/veDelegation.ts
@@ -1,1 +1,25 @@
-export function handleDelegation(): void {}
+import { DelegateBoost } from '../@types/veDelegation/veDelegation'
+import { getveDelegation } from './utils/veUtils'
+
+export function handleDelegation(event: DelegateBoost): void {
+  const _delegator = event.params._delegator.toHex()
+  const _receiver = event.params._receiver.toHex()
+  const _tokenId = event.params._token_id
+  const _amount = event.params._amount
+  const _cancelTime = event.params._cancel_time
+  const _expireTime = event.params._expire_time
+
+  const veDelegation = getveDelegation(_tokenId.toHex())
+  veDelegation.delegator = _delegator
+  veDelegation.receiver = _receiver
+  veDelegation.tokenId = _tokenId
+  veDelegation.amount = _amount
+  veDelegation.cancelTime = _cancelTime
+  veDelegation.expireTime = _expireTime
+  veDelegation.save()
+}
+
+// TODO add handlers for
+// ExtendBoost
+// TransferBoost
+// BurnBoost
