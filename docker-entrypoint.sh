10e357628177d272e08e2e4895fa949cfdf46298
Alex Coseru <alex.coseru@gmail.com>
2023-05-18 16:45:03 +0300

    Add ready state

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,8 @@ echo "deploy subgraph is ${DEPLOY_SUBGRAPH}"
 
 if [ "${DEPLOY_SUBGRAPH}" = "true" ]
 then
+  [ -d /ocean-contracts/subgraph ] || mkdir /ocean-contracts/subgraph
+  rm -f /ocean-contracts/subgraph/ready
   echo "Waiting for contracts to be deployed"
   while [ ! -f "/ocean-contracts/artifacts/ready" ]; do
     sleep 2
@@ -13,5 +15,6 @@ then
   export ADDRESS_FILE="/ocean-contracts/artifacts/address.json"
   cd /usr/src/app/
   npm run quickstart:barge
+  touch /ocean-contracts/subgraph/ready
 fi
 tail -f /dev/null
