cf58c9aae4aa16f7a30b4c93de72786b1d4b3b3f
alexcos20 <alex.coseru@gmail.com>
2023-11-22 09:11:10 +0200

    Release 4.0.1

--- src/mappings/utils/globalUtils.ts
+++ src/mappings/utils/globalUtils.ts
@@ -13,7 +13,7 @@ export function getGlobalStats(): GlobalStatistic {
   let globalStats = GlobalStatistic.load(GLOBAL_ID)
   if (!globalStats) {
     globalStats = new GlobalStatistic(GLOBAL_ID)
-    globalStats.version = '4.0.0'
+    globalStats.version = '4.0.1'
     globalStats.orderCount = 0
     globalStats.fixedCount = 0
     globalStats.datatokenCount = 0
